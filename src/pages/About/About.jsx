7a6d1d04db9e6c99146361df510e11ce640fef1d
CamalPashazade <camal24011992@gmail.com>
2025-04-22 22:13:35 +0400

    changed some codes from home and about pages

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
     <div className={styles.aboute_container}>
 
 
-      <div style={{ width: '100%', height: 'full', backgroundColor: 'slategray' }}>
+      <div className={styles.banner} style={{ width: '100%', height: 'full', backgroundColor: 'slategray' }}>
       <Banner image="/assets/images/istockphoto-1141191007-612x612.jpg"/>
 
       </div>
@@ -27,7 +27,7 @@ const About = () => {
 
    <Title title="About PulseArt"/>
 
-        <p className="text-4        ">
+        <p className={styles.text}>
 
         Grammy Award winning jazz pianist Brad Mehldau has recorded and performed extensively since the early 1990s. Mehldau’s most consistent output over the years has taken place in the trio format. Starting in 1996, his group released a series of five records on Warner Bros. entitled The Art of the Trio (recently re-packaged and re-released as a 5-Disc box set by Nonesuch in late 2011). During that same period, Mehldau also released a solo piano recording entitled Elegiac Cycle, and a record called Places that included both solo piano and trio songs. Elegiac Cycle and Places might be called “concept” albums made up exclusively of original material with central themes that hover over the compositions. Other Mehldau recordings include Largo, a collaborative effort with the innovative musician and producer Jon Brion, and Anything Goes—a trio outing with bassist Larry Grenadier and drummer Jorge Rossy.
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,6 @@
-
 import { Suspense } from 'react';
-import styles from  './Home.module.css';
-import VideoPlayer from '../../components/Videoplayer/VideoPlayer';
+import styles from './Home.module.css';
 import { homepagedata } from '../../datas/homepage/page';
-import useIsMobile from '../../components/Ismobile/isMobile';
-import { Link } from 'react-router-dom';
-
 
 
 function extractYouTubeId(url) {
@@ -14,80 +9,32 @@ function extractYouTubeId(url) {
 }
 
 export default function Home() {
-
-
-
-
-
-  const isMobile = useIsMobile();
-  // const VideoStyle = isMobile && styles.video_mobile ;
-
   return (
     <div className={styles.home_container}>
       {homepagedata.map((item) => (
-
-
-
-
-
         <div
           key={item.id}
           className={styles.Sizes}
           style={{ backgroundColor: item.backcolor }}
         >
-        
-
-      
-
-          <div className={isMobile ? styles.image__mobile_container : styles.image_container}>
+          <div className={styles.image_container}>
             <img
               src={item.img}
               alt="Pulseart_logo"
-          
-           
-             
             />
           </div>
 
-          {/* Video Section */}
-     {/*      <div className={isMobile ? styles.video_container_mobile : styles.video_container}>
-            <Suspense fallback={<div>Loading video...</div>}>
-            <VideoPlayer
-                url={item.videoThumbnail}
-                onPlay={() => console.log(`Video ${item.id} started playing`)}
-                isMobile={isMobile ? styles.video_mobile : styles.video_width}
-            
-            
-              />
-            </Suspense>
-          </div> */}
-          
-
-
-          <div>
-
-
-          <iframe
-  className={isMobile ? styles.video_mobile : styles.video_desktop}
-  src={`https://www.youtube.com/embed/${extractYouTubeId(item.videoThumbnail)}`}
-  title={`YouTube video player ${item.id}`}
-  frameBorder="0"
-  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-  allowFullScreen
-></iframe>
-
-</div>
-
-
-
-
-
+          <div className={styles.video_container}>
+            <iframe
+              className={styles.video_desktop} 
+              src={`https://www.youtube.com/embed/${extractYouTubeId(item.videoThumbnail)}`}
+              title={`YouTube video player ${item.id}`}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
           </div>
-
-
-
-
-      
+        </div>
       ))}
     </div>
   );
